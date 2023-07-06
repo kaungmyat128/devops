5430f24fd544d88e1b9a725547e754d457307f1e
kaungmyat128 <40614801@live.napier.ac.uk>
2023-07-06 20:36:30 +0630

    Initial setup

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,11 @@
+package com.napier.sem;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class App {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+    }
+}
